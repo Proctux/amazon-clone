98460178dc2cb4e9204502e2889bcf59eeb55677
Luiz <luiz.ado@hotmail.com>
2020-09-23 16:58:09 -0300

    Fist commit. Header components created.

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC3eNsR6UbhmN88UyQTjq4PsG3-Sp4tb14",
+  authDomain: "clone-8e1a8.firebaseapp.com",
+  databaseURL: "https://clone-8e1a8.firebaseio.com",
+  projectId: "clone-8e1a8",
+  storageBucket: "clone-8e1a8.appspot.com",
+  messagingSenderId: "906384168302",
+  appId: "1:906384168302:web:c78bfc214172eb09f89d7f",
+  measurementId: "G-GKCSWL0LXX"
+};
