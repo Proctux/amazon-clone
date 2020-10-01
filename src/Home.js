e627f498fbcfc5c63dccfa2424dc554afb70b7ae
Luiz <luiz.ado@hotmail.com>
2020-10-01 08:59:27 -0300

    Added Product component

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.css';
+import Product from './Product';
 
 const Home = () => {
   return (
@@ -8,18 +9,22 @@ const Home = () => {
         <img className="home__image" src="https://store-images.s-microsoft.com/image/apps.16285.14618985536919905.552c0017-6644-49a8-8467-8f7b34ce0428.30ad6b05-16d9-4d5e-a242-43107708a16a?mode=scale&q=90&h=1080&w=1920" alt="" />
 
         <div className="home__row">
-          {/* Product */}
-          {/* Product */}
+          <Product 
+            title="The lean startup"
+            price={29.99}
+            image="https://img.wook.pt/images/lean-startup-eric-ries/MXwxNDc4MDc5NnwxMDI2MzA0OHwxMzgzNTIzMjAwMDAw/250x"
+            rating={5} />
+          <Product />
         </div>
 
         <div className="home__row">
-          {/* Product */}
-          {/* Product */}
-          {/* Product */}
+          <Product />
+          <Product />
+          <Product />
         </div>
 
         <div className="home__row">
-          {/* Product */}
+          <Product />
         </div>
       
       </div>
